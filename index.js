de83258128b58b3af1bb59048d34d68700faf30c
StepanovPlaton <mail2platon@yandex.ru>
2025-02-04 15:09:14 +0400

    Complete LW2.3

--- index.js
+++ index.js
@@ -1,53 +1,59 @@
 // --- #1 ---
-function productOfTwoMax(a, b, c) {
-  let max1, max2;
+const student = {
+  lastName: "Иванов",
+  firstName: "Алексей",
+  marks: [
+    { subject: "Математика", mark: 5 },
+    { subject: "Физика", mark: 4 },
+    { subject: "Химия", mark: 3 },
+  ],
+};
 
-  if (a >= b && a >= c) {
-    max1 = a;
-    max2 = Math.max(b, c);
-  } else if (b >= a && b >= c) {
-    max1 = b;
-    max2 = Math.max(a, c);
-  } else {
-    max1 = c;
-    max2 = Math.max(a, b);
-  }
-
-  return max1 * max2;
+function printStudentInfo(student) {
+  console.log(`Фамилия: ${student.lastName}`);
+  console.log(`Имя: ${student.firstName}`);
+  console.log("Оценки:");
+  student.marks.forEach((mark) => {
+    console.log(`\tПредмет: ${mark.subject}, Оценка: ${mark.mark}`);
+  });
 }
 
-// Example usage:
-console.log(productOfTwoMax(3, 5, 2)); // Outputs: 15
-console.log(productOfTwoMax(7, 1, 4)); // Outputs: 28
+printStudentInfo(student);
 
-// --- #2 ---
-const introduce = (name, rank = "soldier") => `Name: ${name}, Rank: ${rank}`;
+// --- #2 - #5 ---
+class Mark {
+  constructor(subject, mark) {
+    this.subject = subject;
+    this.mark = mark;
+  }
+}
 
-// Example usage:
-console.log(introduce("Max", "admiral")); // Outputs: Name: Max, Звание: admiral
-console.log(introduce("Bob")); // Outputs: Name: Bob, Звание: soldier
+class Student {
+  constructor(lastName, firstName, marks = []) {
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.marks = marks;
+  }
 
-// --- #3 ---
-const executeFunction = (func, ...args) => func(...args);
+  calculateAverageMark() {
+    if (this.marks.length === 0) {
+      return 0;
+    }
 
-// Example usage:
-const add = (a, b) => a + b;
-const result = executeFunction(add, 5, 3);
-console.log(result); // Outputs: 8
+    const total = this.marks.reduce((sum, mark) => sum + mark.mark, 0);
+    return total / this.marks.length;
+  }
 
-// --- #4 ---
-const getPowerFunction = (number) =>
-  number % 2 === 0 ? (n) => n ** 2 : (n) => n ** 3;
+  getMarksBySubject = (subject) =>
+    this.marks
+      .filter((mark) => mark.subject === subject)
+      .map((mark) => mark.mark);
 
-// Example usage:
-const squareFunction = getPowerFunction(4);
-console.log(squareFunction(4)); // Outputs: 16
-const cubeFunction = getPowerFunction(3);
-console.log(cubeFunction(3)); // Outputs: 27
+  addMark(subject, mark) {
+    this.marks.push(new Mark(subject, mark));
+  }
 
-// --- #5 ---
-const factorial = (n) => (n === 0 || n === 1 ? 1 : n * factorial(n - 1));
-
-// Example usage:
-console.log(factorial(5)); // Outputs: 120
-console.log(factorial(0)); // Outputs: 1
+  removeMarksBySubject(subject) {
+    this.marks = this.marks.filter((mark) => mark.subject !== subject);
+  }
+}
